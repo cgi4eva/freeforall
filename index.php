cc168a5728643655e85b2f4ad6a9c733d7d7422c
cgi4eva <55970109+cgi4eva@users.noreply.github.com>
2020-12-20 00:36:37 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
 <!DOCTYPE html>
-    <html>
-      <head>
-        <title>PUBMAJOR</title>
-      </head>
-      <body>
-        <div class='container'>
-          <div class='section friendly'>
-            <h1><strong>PUBMAJOR</strong></h1>
-            <div class='article'>
-              <p>Refer to the <a href="Discord:hckr#3509/">documentation</a> Discord</p>
-            </div>
-          </div>
-        </div>
-      </body>
-    </html>
+<html>
+<body>
+
+<h1>PUBMAJOR</h1>
+
+<?php
+echo "est 20!";
+?>
+
+</body>
+</html>
