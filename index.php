413fe149b0a8117e6b04917d53550ecd3048ccef
cgi4eva <55970109+cgi4eva@users.noreply.github.com>
2020-12-19 21:39:50 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+    <html>
+      <head>
+        <title>PUBMAJOR</title>
+      </head>
+      <body>
+        <div class='container'>
+          <div class='section friendly'>
+            <h1><strong>PUBMAJOR</strong></h1>
+            <div class='article'>
+              <p>Refer to the <a href="hckr#3509/">documentation</a> Discord</p>
+            </div>
+          </div>
+        </div>
+      </body>
+    </html>
